f75667a8d99459cce6b1f3786909c2a35671bf4a
Ramon Nascimento dos santos <ramon.dev.rs@gmail.com>
2025-08-21 00:04:01 +0000

    Criação do menu interativo movimento das principais peças do xadrez uso do swith e dos loops for, while, do-while

--- xadrez.c
+++ xadrez.c
@@ -17,6 +17,86 @@ int main() {
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
+    int c, b = 1, r = 1, R = 0, t = 1, escolhaUsuario, movimentoRainha; // declaração de variaveis
+    // Menu interativo
+    printf("**** Movimento das peças de xadrez ****\n");
+    printf("Qual peça você quer mexer? \n");
+    printf("1. - Peão\n");
+    printf("2. - Cavalo\n");
+    printf("3. - Bispo\n");
+    printf("4. - Rainha\n");
+    printf("5. - Rei\n");
+    printf("6. - Torre\n");
+    scanf("%d", &escolhaUsuario);
+
+
+    switch (escolhaUsuario)
+    {
+    case 1:
+        printf("Movimento do Peão\n");
+        // uso do for com atribuição de variavel local
+        // Peão anda duas casas para cima
+        for (int p = 1; p <= 2 ; p++)
+        {
+           printf("Cima\n");
+        }
+        break;
+    case 2:
+        printf("Movimento carvalo: em construção\n");// movimento do cavalo em construção
+    break;
+    case 3:
+        printf("Movimento do Bispo\n");
+
+        while (b <= 5)
+        {
+            printf("Cima e Direita\n"); // Movimento do Bisco 5 casas na Diagonal direita
+            b++;
+        }
+        
+    break;
+    case 4:
+        printf("Digite quantos movimento a rainha vai fazer para a direita\n");// usuario decidi quantas casas a rainha vai andar
+        scanf("%d", &movimentoRainha);
+        do
+        {
+            printf("<- Esquerda \n");
+            r++;
+        } while (r <= movimentoRainha);
+
+    break;
+    case 5:
+        
+        do
+        {
+            printf("Movimento Rei\n");// Rei anda uma casa pra cima
+            printf("Cima\n");
+            R++;
+        } while (R == 2);  // condição falso para o rei so andar uma casa  
+
+    break;
+    case 6:
+        printf("Movimento da Torre\n"); // torre anda 5 casas para a direita
+
+        while (t <= 5)
+        {
+           printf("Direita ->\n");
+           t++;
+        }
+        
+
+
+    break;
+
+    default:
+        break;
+    }
+
+
+
+
+
+
+
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
